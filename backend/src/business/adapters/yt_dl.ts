efb3d9e2abd9f1130d4612d0e637cc9c3565c573
Federico De Santi <federico.ds25@gmail.com>
2024-01-27 12:13:12 +0100

    feat(youtube-dl): yt-dl exec works

--- backend/src/business/adapters/yt_dl.ts
+++ backend/src/business/adapters/yt_dl.ts
@@ -1,5 +1,8 @@
 import express from "express";
 import youtubedl from "youtube-dl-exec";
+//const youtubedl = require("youtube-dl-exec");
+import logger from 'progress-estimator'
+import { ChildProcess } from "child_process";
 
 
 const ytDlController = async (
@@ -8,25 +11,28 @@ const ytDlController = async (
 ) => {
     try {
         const url: string = req.body.url;
+        console.log("url taken from request: " + url)
         if (!url) {
             return res.status(400).send({ error: "link is required" });
-        }   
+        }
 
-        youtubedl(url, {
-            dumpSingleJson: true,
+        // rischio con await di bloccare il server
+        await youtubedl(url, {
             extractAudio: true,
             audioFormat: "mp3",
             audioQuality: 0,
             output: "%(title)s.%(ext)s",
-            
-        }).then((output: any) => console.log(output));
+        }).then(output => console.log(output));
 
         res.status(200).send("Video scaricato correttamente");
 
     } catch (error) {
-        console.log(error)
-        return res.status(500).send("Non sono riuscito a scaricare il video al link specificato");
+        console.log(error);
+        return res
+            .status(500)
+            .send("Non sono riuscito a scaricare il video al link specificato");
     }
 };
+
 
 export default ytDlController;
