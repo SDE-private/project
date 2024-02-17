817b1a0cb082e34082bc283565be67cbc70f7284
Federico De Santi <federico.ds25@gmail.com>
2024-02-17 18:40:29 +0100

    docs: analyzer

--- backend/src/process/routes/analyzer.ts
+++ backend/src/process/routes/analyzer.ts
@@ -4,6 +4,51 @@ import { User } from "../../business/adapters/db_controller.js";
 
 const analyzeRouter = Router();
 
+/**
+ * @swagger
+ *  /analyze/split/{id}:
+ *     post:
+ *       tags:
+ *         - Analyze
+ *       summary: Split audio file
+ *       description: Split an audio file into its component tracks
+ *       parameters:
+ *         - in: path
+ *           name: id
+ *           required: true
+ *           schema:
+ *             type: string
+ *             description: ID of the audio file
+ *             example: "HsLqNFIFxnk"
+ *       responses:
+ *          '200':
+ *            description: Successfully split audio file
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: object
+ *                  properties:
+ *                    stems:
+ *                      type: object
+ *                      properties:   
+ *                         accompaniment:       
+ *                            type: string       
+ *                            example: "/media/HsLqNFIFxnk/accompaniment.wav"
+ *                         vocals:       
+ *                            type: string       
+ *                            example: "/media/HsLqNFIFxnk/vocals.wav"
+ *                      
+ *          '500':
+ *            description: Server encountered a problem
+ *            content:
+ *              application/json:
+ *                schema:
+ *                  type: object
+ *                  properties:
+ *                    error:
+ *                      type: string
+ *                      example: "Internal Server Error"
+ */
 analyzeRouter.post("/split/:id", check, async (req, res) => {
   const result = await fetch(`http://localhost:3000/spleeter`, {
     method: "POST",
